0dd6f743f0473d9b09e68e668dbced2a130b2fa7
Daniel Lee <dblee0532@gmail.com>
2023-04-20 23:24:03 +0900

    changed responsive for navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -13,13 +13,13 @@ function NavBar({ darkMode, onClick }: Props): JSX.Element {
       <br />
       <div className="flex justify-between items-center mb-8">
         <ul className="list-none">
-          <li className="inline-block pr-12 py-2 hover:font-semibold hover:italic">
+          <li className="inline-block pr-8 sm:pr-16 md:pr-20 lg:pr-20 py-2 hover:font-semibold hover:italic">
             <Link to="/">Home</Link>
           </li>
-          <li className="inline-block pr-12 py-2 hover:font-semibold hover:italic">
+          <li className="inline-block pr-8 sm:pr-16 md:pr-20 lg:pr-20 py-2 hover:font-semibold hover:italic">
             <Link to="/about">About</Link>
           </li>
-          <li className="inline-block pr-12 py-2 hover:font-semibold hover:italic">
+          <li className="inline-block pr-8 sm:pr-16 md:pr-20 lg:pr-20 py-2 hover:font-semibold hover:italic">
             <Link to="/resume">Resume</Link>
           </li>
           <li className="inline-block py-2 hover:font-semibold hover:italic">
